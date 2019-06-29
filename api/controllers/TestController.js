585bde2376c85b3272fd1db61f75328fe164a4ff
baj9032 <baj9032@gmail.com>
2019-06-29 11:54:03 +0530

    added new route

--- api/controllers/TestController.js
+++ api/controllers/TestController.js
@@ -1,5 +1,8 @@
 module.exports = {
   index: function(req, res) {
-    res.status(200).json({ message: 'done' });
+    res.status(200).json({ message: 'index' });
+  },
+  index1: function (req, res) {
+    res.status(200).json({ message: 'index1' });
   }
 };
